59f2f14bb5599f8c0eb2cafbc2612cd12f85db18
Alexander Ela <ela.alexander@gmail.com>
2018-10-08 22:24:20 -0600

    Add templates for stateful components

---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './CardContainer.css';
+import Card from './Card';
+
+class CardContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div>
+			
+			</div>
+		);
+	}
+}
+
+export default CardContainer;
---
+++ src/components/CardContainer/CardContainer.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardContainer from './CardContainer';
+import { shallow } from 'enzyme';
