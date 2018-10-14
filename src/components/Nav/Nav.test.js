5787fea90dc03494490ffbe808f3233d2aa718df
Dina <dcaraballo26@gmail.com>
2018-10-13 23:56:09 -0600

    Add passing snapshot test to Nav component

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -2,3 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './Nav';
 import { shallow } from 'enzyme';
+
+describe('Nav Component', () => {
+  xit('should render without crashing', () => {
+    const wrapper = shallow(<Nav />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
