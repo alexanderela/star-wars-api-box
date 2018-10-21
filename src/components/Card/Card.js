91ac2c887abd04f80666b3ec84298423fc0cab1f
Dina <dcaraballo26@gmail.com>
2018-10-20 23:08:25 -0600

    Refactor code with eslint

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,43 +2,43 @@ import React from 'react';
 import './Card.css';
 import PropTypes from 'prop-types';
 
-const Card = ({ entry, toggleFavorites, favorites, isFavorite }) => { 
-	const cardProperties = entry.properties.map(property => {
-		// console.log(entry)
-		return (
-			<p key={property.header} className="card-text">
-				<span className="card-header">
-					{property.header}
-				</span>
-				{property.text}
-			</p>
-		)
-	})
+const Card = ({
+  entry, toggleFavorites, favorites, isFavorite,
+}) => {
+  const cardProperties = entry.properties.map(property => (
+    <p key={property.header} className="card-text">
+      <span className="card-header">
+        {property.header}
+      </span>
+      {property.text}
+    </p>
+  ));
 
-	return (
-		<div className="Card">
-	    <div className="fav-btn-card-container">
-		    <h3>{entry.name}</h3>
-		    <button 
-		    	className={ `fav-btn 
-		    		${(isFavorite || favorites.includes(entry)) 
-		    			? "fav-btn-active" 
-		    			: "fav-btn-inactive"}` }
-		    	onClick={() => toggleFavorites(entry)}
-		    >
-		    	<i className="fas fa-jedi"></i>
-		  </button>
-	   </div>
-	   {cardProperties}
-		</div>
-	)
-}
+  return (
+    <div className="Card">
+      <div className="fav-btn-card-container">
+        <h3>{entry.name}</h3>
+        <button
+          type="submit"
+          className={`fav-btn 
+						${(isFavorite || favorites.includes(entry))
+            ? 'fav-btn-active'
+            : 'fav-btn-inactive'}`}
+          onClick={() => toggleFavorites(entry)}
+        >
+          <i className="fas fa-jedi" />
+        </button>
+      </div>
+      {cardProperties}
+    </div>
+  );
+};
 
 Card.propTypes = {
-  entry: PropTypes.object,
-  toggleFavorites: PropTypes.func,
-  favorites: PropTypes.array,
-  isFavorite: PropTypes.bool,
+  entry: PropTypes.shape({}).isRequired,
+  toggleFavorites: PropTypes.func.isRequired,
+  favorites: PropTypes.shape([]).isRequired,
+  isFavorite: PropTypes.bool.isRequired,
 };
 
 export default Card;
