d2e16545cd488433756d9b7b2ae5dec05be17ea3
Alexander Ela <ela.alexander@gmail.com>
2018-10-21 14:50:55 -0600

    Add passing tests for App functions

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -76,15 +76,22 @@ beforeEach(() => {
 	mockEvent = { target: true }
 })
 
+afterEach(() => {
+	localStorage.clear()
+})
 
 describe('App component', () => {
 	it('matches the snapshot', () => {
 		expect(wrapper).toMatchSnapshot();
 	})
 
-	it('has the correct default state for films', () => {
+	it('has the correct default state for all categories', () => {
 		expect(wrapper.state().films).not.toBe('{}')
-	}) //Does this apply to the App component itself or a function within the App?
+		expect(wrapper.state().people).toEqual([])
+		expect(wrapper.state().vehicles).toEqual([])
+		expect(wrapper.state().planets).toEqual([])
+		expect(wrapper.state().favorites).toEqual([])
+	})
 
 	it('passes the correct favoritesCount props to Nav', () => {
 		wrapper.setState({ favorites: [{name: 'alvin', type: 'chipmunk'}] })
@@ -105,8 +112,68 @@ describe('App component', () => {
 			wrapper.instance().componentDidMount()
 			expect(wrapper.instance().getFilm).toHaveBeenCalled()
 		})
+	})
 
+	describe('toggleFavorites', () => {
+		it('adds cards to favorites if not already contained in the favorites array', () => {
+			mockEntry = {
+		        "name": "Sand Cralwer",
+		        "id": "Sand Cralwer",
+		        "isFavorite": false, 
+		        "type":"vehicles",
+		        "properties": [
+							{"header": 'Model: ', "text": "Digger Crawler"},
+							{"header": 'Class: ', "text": "wheeled"},
+							{"header": 'Passengers: ', "text": "1"}
+						] 
+		      }
+			wrapper.setState({ favorites: []})
+			wrapper.instance().toggleFavorites(mockEntry)
+			expect(wrapper.state().favorites).toEqual([mockEntry])
+		})
 
+		it('removes cards from favorites if they are already in favorites array', () => {
+			expected = [{
+          name: "Dina Caraballo",
+          id: "Dina Caraballo",
+          isFavorite: true, 
+          type:"people",
+          properties: [
+						{header: 'Homeworld: ', text: "Earth"},
+						{header: 'Species: ', text: "human"},
+						{header: 'Population: ', text: "7,000,000,000"},
+						{header: 'Language: ', text: "Galactic Basic"}
+					] 
+        },
+        {
+          name: "Alex Ela",
+          id: "Alex Ela",
+          isFavorite: true, 
+          type:"people",
+          properties: [
+						{header: 'Homeworld: ', text: "Earth"},
+						{header: 'Species: ', text: "human"},
+						{header: 'Population: ', text: "7,000,000,000"},
+						{header: 'Language: ', text: "Galactic Basic"}
+					] 
+        }]
+			mockEntry =	{
+        "name": "Luke Skywalker",
+        "id": "Luke Skywalker",
+        "isFavorite": true, 
+        "type":"people",
+        "properties": [
+					{"header": 'Homeworld: ', "text": "Tatooine"},
+					{"header": 'Species: ', "text": "human"},
+					{"header": 'Population: ', "text": "200,000"},
+					{"header": 'Language: ', "text": "Galactic Basic"}
+					] 
+        }
+			wrapper.setState({ favorites: mockFavs })
+			wrapper.instance().toggleFavorites(mockEntry)
+			expect(wrapper.state().favorites).toEqual(expected)
+		})
+	})
 	// describe('toggleFavorites', () => {
 	// 	it('adds cards to favorites if not already contained in the favorites array', () => {
 	// 		const card = { "name": "Luke Skywalker", "species": "human" }
@@ -123,7 +190,7 @@ describe('App component', () => {
 		// 	wrapper.instance().removeFromFavorites(mockId)
 		// 	expect(wrapper.state().favorites).toEqual(expected)
 		// })
-	})
+
 
 	describe('isInFavorites', () => {
 		it('should find a match for the given entry', () => {
@@ -188,32 +255,62 @@ describe('App component', () => {
 	})
 
 	describe('getPeople', () => {
-		xit('should call getPerson', () => {
-
+		beforeEach(() => {
+			const mockDataCleaner = new DataCleaner
+			wrapper.setState({ dataCleaner: mockDataCleaner})
 		})
 
-		xit('should setState', () => {
-			
+		it('should call getPerson', async () => {
+			const spy = spyOn(wrapper.state().dataCleaner, 'getPerson')
+			await wrapper.instance().getPeople()
+			await expect(spy).toHaveBeenCalled()
+		})
+
+		it('should setState', async () => {
+			const spy = spyOn(wrapper.state().dataCleaner, 'getPerson')
+			expected = await wrapper.state().dataCleaner.getPerson()
+			await wrapper.instance().getPeople()
+			await expect(wrapper.state().people).toEqual(expected)
 		})
 	})
 
 	describe('getPlanets', () => {
-		xit('should call getPlanet', () => {
-
+		beforeEach(() => {
+			const mockDataCleaner = new DataCleaner
+			wrapper.setState({ dataCleaner: mockDataCleaner})
 		})
 
-		xit('should setState', () => {
-			
+		it('should call getPlanet', async () => {
+			const spy = spyOn(wrapper.state().dataCleaner, 'getPlanet')
+			await wrapper.instance().getPlanets()
+			await expect(spy).toHaveBeenCalled()
+		})
+
+		it('should setState', async () => {
+			const spy = spyOn(wrapper.state().dataCleaner, 'getPlanet')
+			expected = await wrapper.state().dataCleaner.getPlanet()
+			await wrapper.instance().getPlanets()
+			await expect(wrapper.state().planets).toEqual(expected)
 		})
 	})
 
 	describe('getVehicles', () => {
-		xit('should call getVehicle', () => {
-
+		beforeEach(() => {
+			const mockDataCleaner = new DataCleaner
+			wrapper.setState({ dataCleaner: mockDataCleaner})
 		})
 
-		xit('should setState', () => {
-			
+		it('should call getVehicle', async () => {
+			const spy = spyOn(wrapper.state().dataCleaner, 'getVehicle')
+			await wrapper.instance().getVehicles()
+			await expect(spy).toHaveBeenCalled()
+		})
+
+		it('should setState', async () => {
+			const spy = spyOn(wrapper.state().dataCleaner, 'getVehicle')
+			expected = await wrapper.state().dataCleaner.getVehicle()
+			await wrapper.instance().getVehicles()
+			await expect(wrapper.state().vehicles).toEqual(expected)
 		})
 	})
 
@@ -330,7 +427,20 @@ describe('App component', () => {
 			await wrapper.instance().showPeople()
 		  await expect(wrapper.state().people).toEqual(mockLuke)
 		})
+		
+		it('calls getVehicles if there are no vehicles in local storage', async () => {
+			const spy = spyOn(wrapper.instance(), 'showVehicles')
+			localStorage.clear()
+			wrapper.instance().showVehicles()
+			expect(spy).toHaveBeenCalled()
+		})
 
+		it('retrieves vehicles from local storage if there are vehicles in local storage', async () => {
+			localStorage.setItem('vehicles', JSON.stringify(mockFavs))
+			const spy = spyOn(wrapper.instance(), 'getLocalStorage')	
+			wrapper.instance().showVehicles()	
+			expect(spy).toHaveBeenCalled()				
+		})
 		// it('renders a card container displaying people if peopleSelected is true', () => {
 		// 	// wrapper.setState({ peopleSelected: true })
 		//   // const spy = spyOn(wrapper.instance(), 'showPeople');
@@ -361,24 +471,39 @@ describe('App component', () => {
 		// 	await expect(wrapper.state().vehiclesSelected).toBe(false)
 		// })
 
+		beforeEach(() => {
+			mockVehicle = {
+          "name": "Sand Cralwer",
+          "id": "Sand Cralwer",
+          "isFavorite": false, 
+          "type":"vehicles",
+          "properties": [
+						{"header": 'Model: ', "text": "Digger Crawler"},
+						{"header": 'Class: ', "text": "wheeled"},
+						{"header": 'Passengers: ', "text": "1"}
+					] 
+        }
+		})
 		it('sets vehicles to state ', async () => {
-				mockVehicle = {
-	          "name": "Sand Cralwer",
-	          "id": "Sand Cralwer",
-	          "isFavorite": false, 
-	          "type":"vehicles",
-	          "properties": [
-							{"header": 'Model: ', "text": "Digger Crawler"},
-							{"header": 'Class: ', "text": "wheeled"},
-							{"header": 'Passengers: ', "text": "1"}
-						] 
-	        }
 			wrapper.state().dataCleaner.getVehicle = jest.fn().mockImplementation(() => Promise.resolve(
 		 		mockVehicle))
 			await wrapper.instance().showVehicles()
 		  await expect(wrapper.state().vehicles).toEqual(mockVehicle)
 		})
 
+		it('calls getPeople if there are no people in local storage', async () => {
+			const spy = spyOn(wrapper.instance(), 'showPeople')
+			localStorage.clear()
+			wrapper.instance().showPeople()
+			expect(spy).toHaveBeenCalled()
+		})
+
+		it('retrieves people from local storage if there are people in local storage', async () => {
+			localStorage.setItem('people', JSON.stringify(mockVehicle))
+			const spy = spyOn(wrapper.instance(), 'getLocalStorage')	
+			wrapper.instance().showPeople()	
+			expect(spy).toHaveBeenCalled()				
+		})
 		// xit('renders a card container displaying vehicles if vehiclesSelected is true', () => {
 		// 	wrapper.setState({ vehiclesSelected: true })
 		// 	const spy = spyOn(wrapper.instance(), 'showVehicles');
@@ -426,6 +551,19 @@ describe('App component', () => {
 		  await expect(wrapper.state().planets).toEqual(mockPlanet)	
 		})
 
+		it('calls getPlanets if there are no planets in local storage', async () => {
+			const spy = spyOn(wrapper.instance(), 'showPlanets')
+			localStorage.clear()
+			wrapper.instance().showPlanets()
+			expect(spy).toHaveBeenCalled()
+		})
+
+		it('retrieves planets from local storage if there are planets in local storage', async () => {
+			localStorage.setItem('planets', JSON.stringify(mockVehicle))
+			const spy = spyOn(wrapper.instance(), 'getLocalStorage')	
+			wrapper.instance().showPlanets()	
+			expect(spy).toHaveBeenCalled()				
+		})
 		// xit('renders a card container displaying planets if planetsSelected is true', () => {
 		// 	wrapper.setState({ planetsSelected: true })
 		// 	const spy = spyOn(wrapper.instance(), 'showPlanets');
