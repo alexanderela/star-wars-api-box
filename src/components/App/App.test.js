3f0506a5c7081a572f5acc670ed1ba34daa1fc63
Alexander Ela <ela.alexander@gmail.com>
2018-10-21 15:08:58 -0600

    Add passing tests for setting default states in App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -107,10 +107,18 @@ describe('App component', () => {
 	})
 
 	describe('componentDidMount', () => {
-		it('invokes getFilm function on componentDidMount', () => {
+		it('invokes corrects functions on componentDidMount', () => {
 			wrapper.instance().getFilm = jest.fn()
+			wrapper.instance().showPeople = jest.fn()
+			wrapper.instance().showVehicles = jest.fn()
+			wrapper.instance().showPlanets = jest.fn()
+			wrapper.instance().getFavorites = jest.fn()
 			wrapper.instance().componentDidMount()
 			expect(wrapper.instance().getFilm).toHaveBeenCalled()
+			expect(wrapper.instance().showPeople).toHaveBeenCalled()
+			expect(wrapper.instance().showVehicles).toHaveBeenCalled()
+			expect(wrapper.instance().showPlanets).toHaveBeenCalled()
+			expect(wrapper.instance().getFavorites).toHaveBeenCalled()
 		})
 	})
 
@@ -132,7 +140,7 @@ describe('App component', () => {
 			expect(wrapper.state().favorites).toEqual([mockEntry])
 		})
 
-		it('removes cards from favorites if they are already in favorites array', () => {
+		it('removes cards from favorites if they are already in favorites array', async () => {
 			expected = [{
           name: "Dina Caraballo",
           id: "Dina Caraballo",
@@ -173,24 +181,8 @@ describe('App component', () => {
 			wrapper.instance().toggleFavorites(mockEntry)
 			expect(wrapper.state().favorites).toEqual(expected)
 		})
-	})
-	// describe('toggleFavorites', () => {
-	// 	it('adds cards to favorites if not already contained in the favorites array', () => {
-	// 		const card = { "name": "Luke Skywalker", "species": "human" }
-	// 		const expected = [card]
-	// 		wrapper.instance().addToFavorites(card)
-	// 		expect(wrapper.state().favorites).toEqual(expected)
-	// 	})
-	
-		// it('removes cards from favorites array', () => {
-		// 	expected = []
-		// 	const mockId = "Luke"
-		// 	const mockCard = [{ "name": "Luke", "id": "Luke" }]
-		// 	wrapper.setState({ favorites: mockCard })
-		// 	wrapper.instance().removeFromFavorites(mockId)
-		// 	expect(wrapper.state().favorites).toEqual(expected)
-		// })
 
+	})
 
 	describe('isInFavorites', () => {
 		it('should find a match for the given entry', () => {
@@ -314,67 +306,6 @@ describe('App component', () => {
 		})
 	})
 
-	// describe('showFilm', () => {
-	// 	it('sets film to state', async () => {
-	// 		mockFilm = {
-	// 			opening_crawl: "Heyy youu guyyyys", 
-	// 			episode_id: 7, 
-	// 			title: "The Force Awakens"
-	// 		}
-	// 		wrapper.state().dataCleaner.getMovie = jest.fn().mockImplementation(() => Promise.resolve(
-	// 			mockFilm))
-	// 		await wrapper.instance().showFilm()
-	// 		await expect(wrapper.state().films).toEqual(mockFilm)
-	// 	})
-	// })
-
-	// describe('toggleCategoryState', () => {
-	// 	xit('selects people in state when toggleCategory is called', () => {
-	// 		const category = 'people'
-	// 		wrapper.setState({ peopleSelected: false })
-	// 		wrapper.instance().toggleCategoryState(category)
-	// 		expect(wrapper.state().peopleSelected).toEqual(true)
-	// 	})
-
-	// 	xit('invokes showPeople when toggleCategory is called', () => {
-	// 		const instance = wrapper.instance()
-	// 		const category = 'people'
-	// 		const spy = jest.spyOn(instance, 'showPeople')
-	// 		wrapper.instance().toggleCategoryState(category)
-	// 		expect(spy).toHaveBeenCalled()
-	// 	})
-
-	// 	xit('selects vehicles in state when toggleCategory is called', () => {
-	// 		const category = 'vehicles'
-	// 		wrapper.setState({ vehiclesSelected: false })
-	// 		wrapper.instance().toggleCategoryState(category)
-	// 		expect(wrapper.state().vehiclesSelected).toEqual(true)
-	// 	})
-
-	// 	xit('invokes showVehicles when toggleCategory is called', () => {
-	// 		const instance = wrapper.instance()
-	// 		const category = 'vehicles'
-	// 		const spy = jest.spyOn(instance, 'showVehicles')
-	// 		wrapper.instance().toggleCategoryState(category)
-	// 		expect(spy).toHaveBeenCalled()
-	// 	})
-
-	// 	xit('selects planets in state when toggleCategory is called', () => {
-	// 		const category = 'planets'
-	// 		wrapper.setState({ planetsSelected: false })
-	// 		wrapper.instance().toggleCategoryState(category)
-	// 		expect(wrapper.state().planetsSelected).toEqual(true)
-	// 	})
-
-	// 	xit('invokes showPlanets when toggleCategory is called', () => {
-	// 		const instance = wrapper.instance()
-	// 		const category = 'planets'
-	// 		const spy = jest.spyOn(instance, 'showPlanets')
-	// 		wrapper.instance().toggleCategoryState(category)
-	// 		expect(spy).toHaveBeenCalled()
-	// 	})
-	// })
-
 	describe('setLocalStorage', () => {
 		it('sets people data to local storage', () => {
 			const mockData = [{ "class": "wheeled", "model": "Digger Crawler" }, 
@@ -398,17 +329,6 @@ describe('App component', () => {
 	})
 
 	describe('showPeople', () => {
-		// it('should toggle the state of people if it is already selected', () => {
-		// 	mockFilm = {
-		// 		opening_crawl: "Heyy youu guyyyys", 
-		// 		episode_id: 7, 
-		// 		title: "The Force Awakens"
-		// 	}
-		// 	wrapper.setState({ peopleSelected: true, films: mockFilm })
-		// 	wrapper.instance().showPeople()
-		// 	expect(wrapper.state().peopleSelected).toBe(false)
-		// })
-
 		it('sets people to state ', async () => {
 			mockLuke = {
         "name": "Luke Skywalker",
@@ -441,36 +361,9 @@ describe('App component', () => {
 			wrapper.instance().showVehicles()	
 			expect(spy).toHaveBeenCalled()				
 		})
-		// it('renders a card container displaying people if peopleSelected is true', () => {
-		// 	// wrapper.setState({ peopleSelected: true })
-		//   // const spy = spyOn(wrapper.instance(), 'showPeople');
-		//   wrapper.instance().showPeople()
-		// 	// expect(spy).toHaveBeenCalled()
-		// })
 	})
 
-	// describe('checkLocalStoragePeople', () => {
-	// 	it('gets people data from local storage if it already exists there', () => {
-	// 		const mockPeople = [{ "name": "Luke", "species": "human" }]
-	// 		wrapper.instance().setLocalStorage('people', mockPeople)	
-	// 		wrapper.instance().checkLocalStoragePeople(mockPeople)
-	// 		expect(wrapper.state().people).toEqual(mockPeople)
-	// 		localStorage.clear()
-	// 	})
-	// })
-
 	describe('showVehicles', () => {
-		// xit('should toggle the state of vehicles if it is already selected', async () => {
-		// 	mockFilm = {
-		// 		opening_crawl: "Heyy youu guyyyys", 
-		// 		episode_id: 7, 
-		// 		title: "The Force Awakens"
-		// 	}
-		// 	wrapper.setState({ vehiclesSelected: true, films: mockFilm })
-		// 	await wrapper.instance().showVehicles()
-		// 	await expect(wrapper.state().vehiclesSelected).toBe(false)
-		// })
-
 		beforeEach(() => {
 			mockVehicle = {
           "name": "Sand Cralwer",
@@ -504,41 +397,9 @@ describe('App component', () => {
 			wrapper.instance().showPeople()	
 			expect(spy).toHaveBeenCalled()				
 		})
-		// xit('renders a card container displaying vehicles if vehiclesSelected is true', () => {
-		// 	wrapper.setState({ vehiclesSelected: true })
-		// 	const spy = spyOn(wrapper.instance(), 'showVehicles');
-		// 	wrapper.instance().showVehicles()
-		// 	expect(spy).toHaveBeenCalled()
-		// })
 	})
 
-	// describe('checkLocalStorageVehicles', () => {
-	// 	it('gets vehicle data from local storage if it already exists there', () => {
-	// 		mockVehicle = [{
-	// 			"class": "wheeled", 
-	// 			"model": "Digger Crawler",
-	// 			"name": "Sand Crawler", 
-	// 			"passengers": "30"
-	// 		}]
-	// 		wrapper.instance().setLocalStorage('vehicles', mockVehicle)	
-	// 		wrapper.instance().checkLocalStorageVehicles(mockVehicle)
-	// 		expect(wrapper.state().vehicles).toEqual(mockVehicle)
-	// 		localStorage.clear()
-	// 	})
-	// })
-
 	describe('showPlanets', () => {
-		// xit('should toggle the state of planets if it is already selected', async () => {
-		// 	mockFilm = {
-		// 		opening_crawl: "Heyy youu guyyyys", 
-		// 		episode_id: 7, 
-		// 		title: "The Force Awakens"
-		// 	}
-		// 	wrapper.setState({ planetsSelected: true, films: mockFilm })
-		// 	await wrapper.instance().showPlanets()
-		// 	await expect(wrapper.state().planetsSelected).toBe(false)
-		// })
-
 		it('sets planets to state ', async () => {
 			mockPlanet = {"name": "Alderaan", 
 				"terrain": "grasslands, mountains", 
@@ -549,7 +410,7 @@ describe('App component', () => {
 		 		mockPlanet))
 			await wrapper.instance().showPlanets()
 		  await expect(wrapper.state().planets).toEqual(mockPlanet)	
-		})
+	})
 
 		it('calls getPlanets if there are no planets in local storage', async () => {
 			const spy = spyOn(wrapper.instance(), 'showPlanets')
@@ -564,25 +425,5 @@ describe('App component', () => {
 			wrapper.instance().showPlanets()	
 			expect(spy).toHaveBeenCalled()				
 		})
-		// xit('renders a card container displaying planets if planetsSelected is true', () => {
-		// 	wrapper.setState({ planetsSelected: true })
-		// 	const spy = spyOn(wrapper.instance(), 'showPlanets');
-		// 	wrapper.instance().showPlanets()
-		// 	expect(spy).toHaveBeenCalled()	
-		// })
 	})
-
-	// describe('checkLocalStoragePlanets', () => {
-	// 	it('gets planet data from local storage if it already exists there', () => {
-	// 		mockPlanet = [{"name": "Alderaan", 
-	// 			"terrain": "grasslands, mountains", 
-	// 			"population": "2000000000", "climate": "temperate", 
-	// 		"residents": 
-	// 			["Leia Organa", "Bail Prestor Organa", "Raymus Antilles"]}]
-	// 		wrapper.instance().setLocalStorage('planets', mockPlanet)	
-	// 		wrapper.instance().checkLocalStoragePlanets(mockPlanet)
-	// 		expect(wrapper.state().planets).toEqual(mockPlanet)
-	// 		localStorage.clear()
-	// 	})
-	// })
 })
