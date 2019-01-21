e1a07822077c13fc82053fab827fd902d1c9b246
Alexander Ela <ela.alexander@gmail.com>
2019-01-21 16:12:55 -0700

    Updates

---
+++ precache-manifest.d8daf8af594313898879778d5abddcf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5bc7f4f05bc799233d45b4a35fedb09",
+    "url": "/star-wars-api-box/static/media/swapi_rough_gray.a5bc7f4f.jpg"
+  },
+  {
+    "revision": "5d6a625d6647c487bb97e7eeee4ceae9",
+    "url": "/star-wars-api-box/static/media/SWAPI-background.5d6a625d.jpg"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/star-wars-api-box/static/media/Death Star.ade91cb0.otf"
+  },
+  {
+    "revision": "98c7b6e95520f0432414",
+    "url": "/star-wars-api-box/static/js/runtime~main.98c7b6e9.js"
+  },
+  {
+    "revision": "efdc6bc4454f5f8793ff",
+    "url": "/star-wars-api-box/static/js/main.efdc6bc4.chunk.js"
+  },
+  {
+    "revision": "14465d2e0ab656299d3d",
+    "url": "/star-wars-api-box/static/js/1.14465d2e.chunk.js"
+  },
+  {
+    "revision": "efdc6bc4454f5f8793ff",
+    "url": "/star-wars-api-box/static/css/main.49715e58.chunk.css"
+  },
+  {
+    "revision": "dc7c3e5167a4d2f12a6b31ba2a603739",
+    "url": "/star-wars-api-box/index.html"
+  }
+];
